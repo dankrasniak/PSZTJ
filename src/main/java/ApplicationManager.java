0a57ee5fbd8d13ce50b0d7a0b39b08eb676be951
Lukasz Gajowy <lukasz.gajowy@gmail.com>
2014-06-05 20:32:38 +0200

    gui gui gui

--- src/main/java/ApplicationManager.java
+++ src/main/java/ApplicationManager.java
@@ -11,22 +11,23 @@ import parser.Parser;
 import java.util.ArrayList;
 import java.util.Random;
 
-/**
- * Created by Maciej on 2014-05-28.
- */
-public class Psztj {
-    public static void main(String[] args) {
-        Gui applicationGui = new Gui();
-        
+public class ApplicationManager {
+
+    public MotherNature motherNature;
+    public Gui applicationGui;
+
+    public ApplicationManager() {
 
         Parser parser = new Parser();
         Data data = parser.getData();
         Data learningData = new Data();
         Data testingData = new Data();
+
         ArrayList<Input> inputs = data.getInputs();
         ArrayList<Output> outputs = data.getOutputs();
-        for(int i = 0; i < inputs.size(); i++){
-            if(i < 0.8*inputs.size()) {
+
+        for (int i = 0; i < inputs.size(); i++) {
+            if (i < 0.8 * inputs.size()) {
                 learningData.inputData(inputs.get(i), outputs.get(i));
             } else {
                 testingData.inputData(inputs.get(i), outputs.get(i));
@@ -37,9 +38,9 @@ public class Psztj {
         ArrayList<Double> weights;
         ArrayList<Neuron> neurons = new ArrayList<Neuron>();
         ArrayList<NeuralLayer> neuralLayers = new ArrayList<NeuralLayer>();
-        for(int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) {
             weights = new ArrayList<Double>();
-            for(int j = 0; j < 3; j++) {
+            for (int j = 0; j < 3; j++) {
                 weights.add(random.nextDouble());
             }
             neurons.add(new Neuron(new Weights(weights)));
@@ -47,46 +48,59 @@ public class Psztj {
         neuralLayers.add(new NeuralLayer(neurons));
         neurons = new ArrayList<Neuron>();
         weights = new ArrayList<Double>();
-        for(int j = 0; j < 3; j++) {
+        for (int j = 0; j < 3; j++) {
             weights.add(random.nextDouble());
         }
         neurons.add(new Neuron(new Weights(weights)));
         neuralLayers.add(new NeuralLayer(neurons));
 
         NeuralNetwork neuralNetwork = new NeuralNetwork(neuralLayers);
-        MotherNature motherNature = new MotherNature(neuralNetwork, 20, learningData, testingData);
+        motherNature = new MotherNature(neuralNetwork, 20, learningData, testingData);
         System.out.println("Poczatkowe: ");
         motherNature.printPopulationQualities();
-        /*motherNature.nextEpoch(0.5);
-        System.out.println("Nowe: ");
-        motherNature.printPopulationQualities();
-        motherNature.nextEpoch(0.5);
-        System.out.println("Nowe: ");
-        motherNature.printPopulationQualities();
-        motherNature.nextEpoch(0.5);
-        System.out.println("Nowe: ");
-        motherNature.printPopulationQualities();
-        motherNature.nextEpoch(0.5);
-        System.out.println("Nowe: ");
-        motherNature.printPopulationQualities();*/
-        //motherNature.printPopulationQualities();
-        double mutationIntensity = 1.0;
-        for(int i = 0; i < 1000; i++) {
-            motherNature.nextEpoch();
-        }
+
+//        for(int i = 0; i < 1000; i++) {
+//            motherNature.nextEpoch();
+//        }
 
         System.out.println("Nowe: ");
         motherNature.printPopulationQualities();
 
         System.out.println("Przetestowane: ");
-        ArrayList<Double> qualities = (ArrayList)motherNature.getTestedQualities();
-        for(Double quality : qualities)
-        {
+        ArrayList<Double> qualities = (ArrayList) motherNature.getTestedQualities();
+        for (Double quality : qualities) {
             System.out.println(quality);
         }
 
+        applicationGui = new Gui(this);
+        applicationGui.setStartingPopulationTextArea(motherNature.getQualities());
+    }
 
+    public void runAutomatically(int iterations, double startingPression, double endingPression) {
+        double pression = calculatePression(iterations, startingPression, endingPression);
+        for (int i = 0; i < iterations; i++) {
+            step(pression);
+        }
+    }
 
+    public void runOnce(int iterations, double startingPression, double endingPression) {
+        double pression = calculatePression(iterations, startingPression, endingPression);
 
     }
+
+    private double calculatePression(int iterations, double startingPression, double endingPression) {
+        return (endingPression - startingPression) / (double) iterations;
+    }
+
+    public void step(double pression) {
+        motherNature.nextEpoch();
+        motherNature.setSelectionPressure(motherNature.getSelectionPressure() + pression);
+        applicationGui.setCurrentPopulationTextArea(motherNature.getQualities());
+    }
+
+    public static void main(String[] args) {
+        new ApplicationManager();
+    }
+
+
 }
--- src/main/java/Gui.java
+++ src/main/java/Gui.java
@@ -1,41 +1,87 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.List;
 
 /**
  * Created by lukasz on 5/31/14.
  */
-public class Gui extends JFrame{
+public class Gui extends JFrame {
 
     private static final String title = "Podstawy Sztucznej Inteligencji";
 
-    private JButton nextEpoch;
+    private JButton nextEpochBtn;
     private JProgressBar progress;
     private JPanel root;
     private JTextField startingSelectionPression;
     private JTextField endingSelectionPression;
     private JTextField epochCount;
     private JCheckBox eliteStrategy;
-    private JButton run;
+    private JButton runAutomaticallyBtn;
     private JTextArea startingPopulationTextArea;
     private JTextArea currentPopulationTextArea;
     private JCheckBox similarMatch;
     private JTextField textField1;
     private JTable epochResults;
 
-    public Gui() {
+    private ApplicationManager manager;
+
+    public Gui(ApplicationManager manager) {
         super(title);
+        this.manager = manager;
         setContentPane(root);
         pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+
+        nextEpochBtn.addActionListener(nextEpochBtnClicked);
+        runAutomaticallyBtn.addActionListener(runAutomaticallyBtnClicked);
     }
 
+    public void setStartingPopulationTextArea(List<Double> population) {
+        setPopulationTextAreaText(startingPopulationTextArea, population);
+    }
 
+    public void setCurrentPopulationTextArea(List<Double> population) {
+        setPopulationTextAreaText(currentPopulationTextArea, population);
+    }
 
-    public void setPopulationTextArea(JTextArea area, List<Double> startingPopulation) {
+    private void setPopulationTextAreaText(JTextArea area, List<Double> population) {
         area.setText("");
-        for (Double fenotype : startingPopulation) {
+        for (Double fenotype : population) {
             area.append(fenotype + "\n");
         }
+    }
+
+    ActionListener nextEpochBtnClicked = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                int iterations = Integer.parseInt(epochCount.getText());
+                double startingPression = Double.parseDouble(startingSelectionPression.getText());
+                double endingPression = Double.parseDouble(endingSelectionPression.getText());
+                manager.runOnce(iterations, startingPression, endingPression);
+            } catch (NumberFormatException ex) {
+                numberFormatErrorMsg();
+            }
+        }
+    };
+
+    ActionListener runAutomaticallyBtnClicked = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                int iterations = Integer.parseInt(epochCount.getText());
+                double startingPression = Double.parseDouble(startingSelectionPression.getText());
+                double endingPression = Double.parseDouble(endingSelectionPression.getText());
+                manager.runAutomatically(iterations, startingPression, endingPression);
+            } catch (NumberFormatException ex) {
+                numberFormatErrorMsg();
+            }
+        }
+    };
+
+    private void numberFormatErrorMsg() {
+        JOptionPane.showMessageDialog(this, "Please provide valid epochs number, starting pression and ending pression!", "Failure", JOptionPane.ERROR_MESSAGE);
     }
 }
